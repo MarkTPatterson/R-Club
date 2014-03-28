235ef4b07683ee98ae5ce3d04d2a055ce54f6e3b
Mark T Patterson <patterson.markt@gmail.com>
2014-03-28 18:04:58 -0400

    Created Reference Example
    Very short program showing you can use "./" to reference subdirectories
    of the current working directory

---
+++ RClub/program/shortcutReferenceToLocalFile.R
@@ -0,0 +1,10 @@
+## Shortcut for referring to files in same directory
+
+# examine current directory:
+getwd()
+
+# set working directory to the program folder:
+setwd("./program")
+
+# make sure this worked:
+getwd()
